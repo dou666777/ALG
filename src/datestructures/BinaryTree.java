c6d001ca9cbd14414fce0830a1a114658956e3ba
lala <lala@163.com>
2017-07-04 18:42:55 +0800

    add binary tree 01

---
+++ src/datestructures/BinaryTree.java
@@ -0,0 +1,22 @@
+package datestructures;
+
+public class BinaryTree {
+	private Node leftnode;
+	private Node rightnode;
+	
+	public BinaryTree() {
+		
+	}
+	
+	public void insert(Node node){
+		
+	}
+	public void delete(Node node){
+		
+	}
+	
+	
+}
+class Node{
+	
+}
